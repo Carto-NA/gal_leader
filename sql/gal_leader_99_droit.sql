02126c74c84add1819e454af87ba955b6743f1a4
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2020-04-22 16:15:00 +0200

    Update gal_leader_99_droit.sql

--- sql/gal_leader_99_droit.sql
+++ sql/gal_leader_99_droit.sql
@@ -9,6 +9,14 @@
 -- Grant : Droits des éléments
 ------------------------------------------------------------------------
 
--- Tables : met_zon.m_zon_compo_gal_leader_na
-GRANT ALL ON TABLE met_zon.m_zon_compo_gal_leader_na TO "pre-sig-usr";
-GRANT SELECT ON TABLE met_zon.m_zon_compo_gal_leader_na TO "pre-sig-ro";
+-- Tables : ref_zonage.t_appartenance_geo_com_gal_leader
+GRANT ALL ON TABLE ref_zonage.t_appartenance_geo_com_gal_leader TO "pre-sig-usr";
+GRANT SELECT ON TABLE ref_zonage.t_appartenance_geo_com_gal_leader TO "pre-sig-ro";
+
+-- Tables : met_zon.m_zon_lt_gal_leader
+GRANT ALL ON TABLE met_zon.m_zon_lt_gal_leader TO "pre-sig-usr";
+GRANT SELECT ON TABLE met_zon.m_zon_lt_gal_leader TO "pre-sig-ro";
+
+-- Tables : met_zon.m_zon_gal_leader_na_geo
+GRANT ALL ON TABLE met_zon.m_zon_gal_leader_na_geo TO "pre-sig-usr";
+GRANT SELECT ON TABLE met_zon.m_zon_gal_leader_na_geo TO "pre-sig-ro";
